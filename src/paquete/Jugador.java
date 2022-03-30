3ba07e932e22a194b160c15a0f42c5bb889965d1
Ángel <arn0005@alu.medac.es>
2022-03-30 12:03:56 +0200

    Clase jugador terminada

--- src/paquete/Jugador.java
+++ src/paquete/Jugador.java
@@ -9,16 +9,59 @@ public class Jugador extends Persona {
     
     
      */
-    private double altura;
+    private double altura; //en metros
     private Posicion posicion;
     private int partidosJugados;
     private boolean estaJugando;
 
     public Jugador(double altura, Posicion posicion, String nombre, String direccion, String DNI, boolean sexo) {
         super(nombre, direccion, DNI, sexo);
-        this.altura = altura;
-        this.posicion = posicion;
+        if (altura > 1.65) {
+            this.altura = altura;
+        } else {
+            this.altura = 1.70;
+        }
+        if (posicion == Posicion.base) {
+            this.posicion = Posicion.base;
+        } else if (posicion == Posicion.alapivot) {
+            this.posicion = Posicion.alapivot;
+        } else if (posicion == Posicion.escolta) {
+            this.posicion = Posicion.escolta;
+        } else if (posicion == Posicion.pivot) {
+            this.posicion = Posicion.pivot;
+        } else {
+            this.posicion = Posicion.alero;
+        }
         this.partidosJugados = 0;
         this.estaJugando = false;
     }
+
+    public void jugando() {
+        if (estaJugando == true) {
+            if (sexo == true) {
+                System.out.println("El jugador " + super.dameNombre() + " está jugando");
+            } else {
+                System.out.println("La jugadora " + super.dameNombre() + " está jugando");
+            }
+        } else {
+            if (sexo == true) {
+                System.out.println("El jugador " + super.dameNombre() + " no está jugando");
+            } else {
+                System.out.println("La jugadora " + super.dameNombre() + " no está jugando");
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String res;
+        res = super.toString() + "\nAltura: " + this.altura + "\nSu posicion es: "
+                + this.posicion + "\nJugador/a en " + this.partidosJugados + " partidos";
+        if (estaJugando == true) {
+            return res + "\nAhora mismo está jugando";
+        } else {
+            return res + "\nAhora mismo no está jugando";
+        }
+    }
+
 }
--- src/paquete/principal.java
+++ src/paquete/principal.java
@@ -11,7 +11,9 @@ public class principal {
      */
     public static void main(String[] args) {
         Persona Angel = new Persona("Angel", "Poeta Agustin", "26836851Z", true);
-        System.out.println(Angel);
+       // System.out.println(Angel);
+        Jugador Angel2 = new Jugador(1.71, Posicion.base, Angel.dameNombre(), Angel.dameDireccion(), Angel.dameDNI(), true);
+        System.out.println(Angel2.toString());
     }
 
 }
